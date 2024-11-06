790267bda924828bde1fa1017d4285b9df44e105
Sniqur <stephansnigur@gmail.com>
2024-11-06 18:37:35 +0200

    azure provider data change

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
   client_id       = "e992153e-51b3-4f54-9361-510b8045568a"
-  client_secret   = "jR~8Q~9F4PtAEOIF9oHjtHbc_d8lMMFSBgibxasW"
+  client_secret   = "b2bdb5ef-229e-4ae5-a38d-9b54bb1972f7"
   tenant_id       = "f128dd7a-1fd1-4e3e-8496-674b767ef552"
   subscription_id = "1a8f5438-2e23-4a1f-b53c-3ebf42a36c48"
 }
