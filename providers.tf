69526e342c7c4e8963daa489a6a21f960d648d6b
Sniqur <stephansnigur@gmail.com>
2024-11-06 18:18:26 +0200

    short

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
   features {}
-
   client_id       = "e992153e-51b3-4f54-9361-510b8045568a"
   client_secret   = "jR~8Q~9F4PtAEOIF9oHjtHbc_d8lMMFSBgibxasW"
   tenant_id       = "f128dd7a-1fd1-4e3e-8496-674b767ef552"
