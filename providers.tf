871e1dff3c8fc3b5cb8ba1dd8b8cbe2ccf6dd8b4
Sniqur <stephansnigur@gmail.com>
2024-11-06 18:43:33 +0200

    testing 1

--- providers.tf
+++ providers.tf
@@ -4,4 +4,5 @@ provider "azurerm" {
   client_secret   = "b2bdb5ef-229e-4ae5-a38d-9b54bb1972f7"
   tenant_id       = "f128dd7a-1fd1-4e3e-8496-674b767ef552"
   subscription_id = "1a8f5438-2e23-4a1f-b53c-3ebf42a36c48"
+  
 }
