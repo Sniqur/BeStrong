3cff13232aaaf73946b3655e52fa8260e6b24c39
Sniqur <stephansnigur@gmail.com>
2024-11-06 18:46:15 +0200

    client secret change

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
   client_id       = "e992153e-51b3-4f54-9361-510b8045568a"
-  client_secret   = "b2bdb5ef-229e-4ae5-a38d-9b54bb1972f7"
+  client_secret   = "ca947249-09f9-45f4-9364-8701fcedca20"
   tenant_id       = "f128dd7a-1fd1-4e3e-8496-674b767ef552"
   subscription_id = "1a8f5438-2e23-4a1f-b53c-3ebf42a36c48"
-  
+
 }
